38fc6213b14bc8726254163423c9630efba69b72
Ivo M. Panayotov <ImPanayotov@github.com>
2020-11-21 23:41:09 +0200

    hr

---
+++ 30-DayChallenge/Day05-Loops/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Day05_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //testing
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{n} x {i} = {n*i}");
+            }
+        }
+    }
+}
