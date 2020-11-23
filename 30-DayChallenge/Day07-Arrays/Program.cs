00a82e651db2ad0bf6e7b50f09d3159abfd31b12
ImPanayotov <i.m.panayotov@gmail.com>
2020-11-24 00:58:21 +0200

    hr

---
+++ 30-DayChallenge/Day07-Arrays/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Day07_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
+            ;
+
+            Array.Reverse(arr);
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+        }
+    }
+}
