653dda0865391c25203fe69635a873c65562d7f2
ImPanayotov <i.m.panayotov@gmail.com>
2020-12-17 00:33:48 +0200

    Update Solution.cs

--- 30-DayChallenge/Day09-Recursion3/Solution.cs
+++ 30-DayChallenge/Day09-Recursion3/Solution.cs
@@ -7,17 +7,11 @@ namespace Day09_Recursion3
     {
         static void Main(string[] args)
         {
-            //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
-
             int n = Convert.ToInt32(Console.ReadLine());
 
             int result = factorial(n);
 
             Console.WriteLine(result);
-            //textWriter.WriteLine(result);
-
-            //textWriter.Flush();
-            //textWriter.Close();
         }
 
         static int factorial(int n)
