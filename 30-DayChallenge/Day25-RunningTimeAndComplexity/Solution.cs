ec1d8c7e8e59ed5ef9809532934d816fdf09ffd8
ImPanayotov <i.m.panayotov@gmail.com>
2020-12-26 01:56:55 +0200

    HR

---
+++ 30-DayChallenge/Day25-RunningTimeAndComplexity/Solution.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Day25_RunningTimeAndComplexity
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                bool isPrime = PrimeOrNot(number);
+
+                if (isPrime)
+                {
+                    Console.WriteLine("Prime");
+                }
+                else
+                {
+                    Console.WriteLine("Not prime");
+                }
+            }
+        }
+
+        private static bool PrimeOrNot(int number)
+        {
+            if (number == 1)
+            {
+                return false;
+            }
+            if (number == 2)
+            {
+                return true;
+            }
+
+            double sqrt = Math.Sqrt(number);
+
+            for (int i = 2; i <= sqrt; ++i)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
