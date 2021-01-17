178d90c2920a0f5b1a46ec0c04233ffdcc68dfc2
ImPanayotov <i.m.panayotov@gmail.com>
2021-01-17 15:36:39 +0200

    HR Certified test

---
+++ CSharp-Basic(Certified)/NotesStore/NotesStore.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NotesStore
+{
+    public class NotesStore
+    {
+        private Dictionary<string, List<string>> notes;
+
+        public NotesStore()
+        {
+            notes = new Dictionary<string, List<string>> 
+            {
+                { "completed", new List<string>() }, 
+                { "active", new List<string>() }, 
+                { "others", new List<string>() } 
+            };
+        }
+
+        public void AddNote(String state, String name)
+        {
+
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new Exception("Name cannot be empty");
+            }
+
+            if (state == "completed")
+            {
+                notes[state].Add(name);
+            }
+            else if (state == "active")
+            {
+                notes[state].Add(name);
+            }
+            else if (state == "others")
+            {
+                notes[state].Add(name);
+            }
+            else
+            {
+                throw new Exception($"Invalid state {state}");
+            }
+        }
+
+        public List<String> GetNotes(String state)
+        {
+            if (state == "completed" || state == "active" || state == "others")
+            {
+                List<string> result = notes[state].GetRange(0, notes[state].Count);
+                
+                return result;
+            }
+
+            throw new Exception($"Invalid state {state}");
+        }
+    }
+}
